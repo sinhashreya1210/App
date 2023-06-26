e30cb88975f041fc315092c2e8f0c4a588429bf2
sinhashreya1210 <46097983+sinhashreya1210@users.noreply.github.com>
2023-06-26 14:57:31 -0500

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -87,14 +87,6 @@ def shortest_path(graph, messages, start, target):
             output += f"\n {edge[0]} -> {edge[1]}: {messages[edge]}\n"
     return output
 
-message = {('P'): 'abc',
-            ('NP'): 'ccd'}
-for i in range(distance):
-        edge = (path[i])
-        if edge in message:
-            output += f"\n {edge[0]} : {message[edge]}\n"
-    return output
-
 # Streamlit app
 st.title("Complexity Class Graph")
 start_class = st.selectbox("Select start class", options=list(graph.keys()))
